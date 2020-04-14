65248786a366d89b3a8b1431ebc8f600f98b935b
zawette <zawette@gmail.com>
2020-04-14 17:51:23 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# SchoolClassReservation
+## notes
++ the API project depends on the "Application" and "Persistance" layers
++ the Application layer contains interfaces, Dtos , command/queries , exceptions and all application logic , it only depends on the domain layer
+    + it defines interfaces that are implemented by outside layers
++ cqrs: seperates reads(queries) from writes(commands)
+    + can maximize the performance , scalability, and simplicity
+    + easy to mantain (changes only affect one command or query)
++ the project is not complex enough to justify using the cqrs pattern, i only implemented it for learning purposes 
++ Domain layer contains entities and value types and logic specific to the domain 
++ the presentation layer can be changed with minimal effort
+## technologies
+ASP.NET Core 3
+Entity Framework Core 
+mediatR
+Identity
--- SchoolClassReservationMVC/Controllers/HomeController.cs
+++ SchoolClassReservationMVC/Controllers/HomeController.cs
@@ -33,13 +33,15 @@ namespace SchoolClassReservationMVC.Controllers
             using var response = await httpClient.GetAsync("https://localhost:44336/api/Reservations");
             string apiResponse = await response.Content.ReadAsStringAsync();
             Reservations = JsonConvert.DeserializeObject<List<ReservationDto>>(apiResponse);
+
+            if (TempData["apiResponse"] != null) ViewBag.Result = TempData["apiResponse"].ToString();
             return View(Reservations);
         }
 
         public async Task<IActionResult> AddReservation()
         {
             var reservationCreationVM = await ReservationCreationVM.getFromApiAsync();
-            if (TempData["apiResponse"]!=null) ViewBag.Error = TempData["apiResponse"].ToString();
+            if (TempData["apiResponse"] != null) ViewBag.Error = TempData["apiResponse"].ToString();
 
             return View(reservationCreationVM);
         }
@@ -62,8 +64,9 @@ namespace SchoolClassReservationMVC.Controllers
 
             if (response.IsSuccessStatusCode)
             {
-                var receivedReservation = new Reservation();
-                receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                //var receivedReservation = new Reservation();
+                //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                TempData["apiResponse"] = "Reservation created successfully";
                 return RedirectToAction("Index");
             }
             else
@@ -72,6 +75,78 @@ namespace SchoolClassReservationMVC.Controllers
                 return RedirectToAction("AddReservation");
             }
         }
+
+        public async Task<IActionResult> DeleteReservation(string id)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.DeleteAsync($"https://localhost:44336/api/Reservations/{id}");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["apiResponse"] = "Reservation deleted successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["apiResponse"] = apiResponse;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        public async Task<IActionResult> UpdateReservation(string id)
+        {
+            var reservationCreationVM = await ReservationCreationVM.getFromApiAsync();
+
+
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync($"https://localhost:44336/api/Reservations/{id}");
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Reservation = JsonConvert.DeserializeObject<ReservationDto>(apiResponse);
+                return View(reservationCreationVM);
+            }
+            else
+            {
+                TempData["apiResponse"] = apiResponse;
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReservation(string id,ReservationCreationVM reservation)
+        {
+            var reservationToSend = new Reservation
+            {
+                coursId = reservation.coursId,
+                salleId = reservation.salleId,
+                instructorId = reservation.instructorId,
+                dateDebut = reservation.dateDebut,
+                dateFin = reservation.dateFin
+            };
+            using var httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(reservationToSend), Encoding.UTF8, "application/json");
+            using var response = await httpClient.PutAsync($"https://localhost:44336/api/Reservations/{id}", content);
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                //var receivedReservation = new Reservation();
+                //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                TempData["apiResponse"] = "Reservation updated successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["apiResponse"] = apiResponse;
+                return RedirectToAction("Index");
+            }
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
